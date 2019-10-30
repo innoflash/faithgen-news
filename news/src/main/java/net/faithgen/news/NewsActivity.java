e0d947336544d11b6b3bfdef2c92019cdc377cc1
innoflash <innocentmazando@gmail.com>
2019-10-30 09:08:32 +0200

    added searchliveo library

--- news/src/main/java/net/faithgen/news/NewsActivity.java
+++ news/src/main/java/net/faithgen/news/NewsActivity.java
@@ -22,5 +22,9 @@ public class NewsActivity extends FaithGenActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+
+        setOnOptionsClicked(R.drawable.ic_search_black_24dp, view -> {
+
+        });
     }
 }
