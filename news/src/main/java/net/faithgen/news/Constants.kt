27953e37d8d4a482d4ea23065c9dad4d8876ffe6
innoflash <innocentmazando@gmail.com>
2020-03-03 12:33:40 +0200

    converted constants to Kotlin

---
+++ news/src/main/java/net/faithgen/news/Constants.kt
@@ -0,0 +1,11 @@
+package net.faithgen.news
+
+object Constants {
+    const val NEWS = "News"
+    const val FILTER_TEXT = "filter_text"
+    const val NEWS_ROUTE = "news"
+    const val ARTICLE = "Article"
+    const val SHARE = "Share"
+    const val COMMENT = "Comment"
+    const val ARTICLE_OPTIONS = "Article Options"
+}
