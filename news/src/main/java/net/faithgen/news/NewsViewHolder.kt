cdc0c0f0c153ed816f0635a550ccabbb2ef95004
innoflash <innocentmazando@gmail.com>
2020-03-03 12:47:33 +0200

    changed news holder

---
+++ news/src/main/java/net/faithgen/news/NewsViewHolder.kt
@@ -0,0 +1,10 @@
+package net.faithgen.news
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import net.innoflash.iosview.lists.ListItemView3
+
+/**
+ * The news items holder
+ */
+final class NewsViewHolder(val newsView: ListItemView3) : RecyclerView.ViewHolder(newsView) {}
